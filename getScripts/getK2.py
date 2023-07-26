5c070e96a71407a24a731fadbf8c235031f2c3ce
Ingrid Pelisoli <phshsc@lsg.astro.warwick.ac.uk>
2023-07-26 10:18:03 +0100

    Changed output to .dat and fixed error for when no data is found

--- getScripts/getK2.py
+++ getScripts/getK2.py
@@ -25,27 +25,27 @@ from astropy.timeseries import LombScargle
 class K2(object):
     def get_K2(RADec, exptime, radius=3, ignore_any_dodgyness=False):
         search_result_lc=lk.search_lightcurve(RADec, radius=radius,author="Kepler", exptime=exptime) # https://docs.lightkurve.org/reference/api/lightkurve.search_lightcurve.html
-        
-        
+
+
         # inspect tpf: https://docs.lightkurve.org/tutorials/1-getting-started/interactively-inspecting-data.html
         if len(search_result_lc)==0:
             search_result = lk.search_targetpixelfile(RADec, mission="Kepler", exptime=exptime)
             if search_result:
                 all_tpf = search_result.download_all()
                 all_lcs=[]
-                
-                
+
+
                 if all_tpf:  # if this is not empty
                     for tpf in all_tpf:
                         try:
                             lc=tpf.to_lightcurve(method="pld") #cbv #pld
                             #print(lc.info())
                             lc=lc.remove_nans()
-                            
+
                             all_lcs.append(lc)
-                            
-                                
-                            
+
+
+
                         except Exception as e:
                             print(e)
                             print("trying to deal with nans")
@@ -59,65 +59,62 @@ class K2(object):
                             else:
                                 ax.figure.savefig('K2_maybeDodgy_butIncludedInPeriodogram.png')
                                 all_lcs.append(lc)
-                        
-                        
+
+
                     lc_obj=lk.LightCurveCollection(all_lcs).stitch().remove_outliers()
                     ax=lc_obj.scatter(title=exptime)
                     ax.figure.savefig('K2_AllLC'+str(exptime)+'.png')
-                        
+
             elif len(search_result_lc) > 1:
                 lc_obj=search_result_lc.stitch().remove_outliers().remove_nans()   # the flux here is meant to automatically be PDCSAP
             plt.close()
-            
-            np.savetxt("K2data_"+str(exptime)+".csv", np.array([lc_obj.time, lc_obj.flux, lc_obj.flux_err]).T, fmt="%s")
-            
+
+            try:
+                np.savetxt("K2_"+str(exptime)+".dat", np.array([lc_obj.time, lc_obj.flux, lc_obj.flux_err]).T, fmt="%s")
+            except:
+                print("No K2 data found.\n")
+
             try: # this will only break if lc_obj does not exist
                 #ts=lc_obj.time
                 #fluxes=lc_obj.flux
                 #fluxes_e=lc_obj.flux_err
-                
+
                 #times_to_sample=np.linspace(1/86400,1/250,1000)*u.Hz
                 #ls = LombScargle(ts,fluxes,fluxes_e)
                 #power=ls.power(times_to_sample)
-                
-                
+
+
                 #plt.plot((1/times_to_sample)/86400, power)
                 #plt.show()
-            
-            
-            
-            
+
+
+
+
                 periodogram=lc_obj.to_periodogram("bls", minimum_period=8*60/86400, maximum_period=0.5)
                 plt.clf()
-                
+
                 #plt_periodogram=periodogram.plot()
                 #plt_periodogram.axhline(ls.false_alarm_level(1-0.9987), c="r")
-                
+
                 periodogram.plot().figure.savefig('K2_Periodogram_'+str(exptime)+'.png')
-                
+
                 period=periodogram.period_at_max_power
                 folded_lc=lc_obj.fold(period)
                 folded_lc.scatter(title=str(period) + "   " + str(exptime))
                 folded_lc.scatter(title=str(period) + "   " + str(exptime)).figure.savefig('K2_folded_lc_'+str(exptime)+'.png')
-                
+
                 #folded_lc.plot_river(method='sigma')
                 plt.close()
             except Exception as exc:
                 None#print(exc)
-            
-            
-            
-            
-            
+
+
+
+
+
             #lightkurve.LightCurve.create_transit_mask
-            
+
             # for kepler data, https://docs.lightkurve.org/tutorials/2-creating-light-curves/2-3-k2-sffcorrector.html
-            
-            
+
+
             # limitation: might struggle with crowded fields... one can use difference imaging with lightkurve https://docs.lightkurve.org/tutorials/3-science-examples/periodograms-verifying-the-location-of-a-signal.html
-        
-
-    
-
-    
-            
