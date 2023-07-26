075701e9d33f79d4044b0a73990b51ac508a5799
Ingrid Pelisoli <phshsc@lsg.astro.warwick.ac.uk>
2023-07-26 14:17:22 +0100

    Now always saving all bands (trust user to judge their usability); changed filenames for consistency.

--- getScripts/getWISE.py
+++ getScripts/getWISE.py
@@ -16,8 +16,8 @@ from astropy.coordinates import SkyCoord, EarthLocation
 from miscAstro import * # analysis:ignore
 import dateutil
 import astropy
-#WISE 5σ photometric sensitivity is estimated to be 0.068, 0.098, 0.86 and 5.4 mJy 
-#(16.6, 15.6, 11.3, 8.0 Vega mag) at 3.4, 4.6, 12 and 22 μm in unconfused regions on the 
+#WISE 5σ photometric sensitivity is estimated to be 0.068, 0.098, 0.86 and 5.4 mJy
+#(16.6, 15.6, 11.3, 8.0 Vega mag) at 3.4, 4.6, 12 and 22 μm in unconfused regions on the
 #ecliptic plane
 
 
@@ -31,94 +31,93 @@ class WISE(object):
         time = astropy.time.Time(dt)
         time2= astropy.time.Time(dt2)
         deltamjd_yr=(time.jd - time2.jd)/365
-        
-        
+
+
         pos_ra_change = ((pmra/3600) /1000)  *(-1*deltamjd_yr)
         RAdeg+=pos_ra_change
-        
+
         pos_dec_change = ((pmdec/3600) /1000)  *(-1*deltamjd_yr)
         Decdeg+=pos_dec_change
         ##############
-        
+
         Irsa.ROW_LIMIT = 5000   # 1000 is the new value for row limit here.
         t = Irsa.query_region(SkyCoord(RAdeg, Decdeg, unit=(u.deg,u.deg)), spatial="Cone",
-                                                            radius=3 * u.arcsec, catalog="allwise_p3as_mep", 
+                                                            radius=3 * u.arcsec, catalog="allwise_p3as_mep",
                                                             selcols="ra,dec,mjd,w1mpro_ep,w1sigmpro_ep,w2mpro_ep,w2sigmpro_ep,w3mpro_ep,w3sigmpro_ep,w4mpro_ep,w4sigmpro_ep")
-        
+
         t.write('WISE.csv', overwrite=True)
 
 
-    def saveFilters(RAdeg,Decdeg,gmag):
+    def saveFilters(RAdeg,Decdeg):
         table=Table.read('WISE.csv')
-        
+
         #IMPORTANT NOTE: WISE WAS A SPACECRAFT AND THE STORED TIME WAS MJD. I DO NOT KNOW THE EXACT LOCATION FOR BJD
-        
-        if gmag<16.6: #not optimal, but if the gmag is 16.6 and i look for white dwarfs, will be brighter in blue
+
+        try:
+            mask1=(table['w1mpro_ep'] > 0)
+            mjd=table['mjd'][mask1]     + 0.57    # this is correct for WISE and NEOWISE W1 and W2
+            bjd=miscAstro.jd_corr(mjd,RAdeg,Decdeg,loc=EarthLocation.of_site("lapalma")).value
+            w1mag=table['w1mpro_ep'][mask1]
+            w1mage=table['w1sigmpro_ep'][mask1]
+            np.savetxt("WISE_W1.dat", np.array([bjd, w1mag, w1mage]).T)
+
             try:
-                mask1=((table['w1mpro_ep'] > 0)      &     (table['w1mpro_ep'] > gmag-2.25))
-                mjd=table['mjd'][mask1]     + 0.57    # this is correct for WISE and NEOWISE W1 and W2
-                bjd=miscAstro.jd_corr(mjd,RAdeg,Decdeg,loc=EarthLocation.of_site("lapalma")).value
-                w1mag=table['w1mpro_ep'][mask1]
-                w1mage=table['w1sigmpro_ep'][mask1]
-                np.savetxt("WISE1_340nm.csv", np.array([bjd, w1mag, w1mage]).T)
-            except: None
-        
-        if gmag<15.6:
-            try:
-                mask2=((table['w2mpro_ep'] > 0)      &     (table['w2mpro_ep'] > gmag-2))
+                mask2=(table['w2mpro_ep'] > 0)
                 mjd=table['mjd'][mask2]      + 0.57    # this is correct for WISE and NEOWISE W1 and W2
                 bjd=miscAstro.jd_corr(mjd,RAdeg,Decdeg,loc=EarthLocation.of_site("lapalma")).value
                 w2mag=table['w2mpro_ep'][mask2]
                 w2mage=table['w2sigmpro_ep'][mask2]
-                np.savetxt("WISE2_460nm.csv", np.array([bjd, w2mag, w2mage]).T)
-            except: None
-        
-        if gmag<10.5:
+                np.savetxt("WISE_W2.dat", np.array([bjd, w2mag, w2mage]).T)
+            except:
+                print("Could not create W2 light curve.")
+
             try:
-                mask3=((table['w3mpro_ep'] > 0)      &     (table['w3mpro_ep'] > gmag-2))
+                mask3=(table['w3mpro_ep'] > 0)
                 mjd=table['mjd'][mask3]
                 bjd=miscAstro.jd_corr(mjd,RAdeg,Decdeg,loc=EarthLocation.of_site("lapalma")).value
                 w3mag=table['w3mpro_ep'][mask3]
                 w3mage=table['w3sigmpro_ep'][mask3]
-                np.savetxt("WISE3_1200nm.csv", np.array([bjd, w3mag, w3mage]).T)
-            except: None
-        
-        if gmag<7:
+                np.savetxt("WISE_W3.dat", np.array([bjd, w3mag, w3mage]).T)
+            except:
+                print("Could not create W3 light curve.")
+
             try:
-                mask4=((table['w4mpro_ep'] > 0)      &     (table['w4mpro_ep'] > gmag-2))
+                mask4=(table['w4mpro_ep'] > 0)
                 mjd=table['mjd'][mask4]
                 bjd=miscAstro.jd_corr(mjd,RAdeg,Decdeg,loc=EarthLocation.of_site("lapalma")).value
                 w4mag=table['w4mpro_ep'][mask4]
                 w4mage=table['w4sigmpro_ep'][mask4]
-                np.savetxt("WISE4_2200nm.csv", np.array([bjd, w4mag, w4mage]).T)
-            except: None
+                np.savetxt("WISE_W4.dat", np.array([bjd, w4mag, w4mage]).T)
+            except:
+                print("Could not create W4 light curve.")
+
+            return len(table['mjd'])
+
+        except:
+            print("Could not create WISE light curves.\n")
+            return None
 
     def Plot():
         try:
-            mjd,mag,mage=np.loadtxt("WISE1_340nm.csv",unpack=True)
+            mjd,mag,mage=np.loadtxt("WISE_W1.dat",unpack=True)
             plt.scatter(mjd,mag, c='b', label="WISE1")
             plt.errorbar(mjd,mag,yerr=mage,ls=" ", c='b')
         except: None
-        
+
         try:
-            mjd,mag,mage=np.loadtxt("WISE2_460nm.csv",unpack=True)
+            mjd,mag,mage=np.loadtxt("WISE_W2.dat",unpack=True)
             plt.scatter(mjd,mag, c='g', label="WISE2")
             plt.errorbar(mjd,mag,yerr=mage,ls=" ", c='g')
         except: None
-        
+
         try:
-            mjd,mag,mage=np.loadtxt("WISE3_1200nm.csv",unpack=True)
+            mjd,mag,mage=np.loadtxt("WISE_W3.dat",unpack=True)
             plt.scatter(mjd,mag, c='orange', label="WISE3")
             plt.errorbar(mjd,mag,yerr=mage,ls=" ", c='orange')
         except: None
-        
+
         try:
-            mjd,mag,mage=np.loadtxt("WISE4_2200nm.csv",unpack=True)
+            mjd,mag,mage=np.loadtxt("WISE_W4.dat",unpack=True)
             plt.scatter(mjd,mag, c='brown', label="WISE4")
             plt.errorbar(mjd,mag,yerr=mage,ls=" ", c='brown')
         except: None
-        
-        
-
-
-
